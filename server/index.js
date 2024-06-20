3e0c6c0f6098c6394964fe2fd01779a89376c0c1
Prabhjot-84 <prabhjotsingh1645@gmail.com>
2024-06-20 22:32:50 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -11,14 +11,17 @@ import { deleteExpenseByID } from './controller/delete.js';
 
 dotenv.config();
 const app = express();
-// const PORT = process.env.PORT || 5000;
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use( cors({credentials:true, origin:'http://localhost:5173'}) );
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose.connect(process.env.CONNECTION_URI);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Expense Tracker API');
+});
 
 app.post('/expense', addExpenseFunc);
 app.get('/expense/:userId', getExpensesByUserID);
