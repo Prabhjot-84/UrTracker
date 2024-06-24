efa7a10789fc92aba2325b0149dafb4679a797fe
Prabhjot-84 <prabhjotsingh1645@gmail.com>
2024-06-24 11:11:46 +0530

    console log backend url

--- client/src/components/shared/Expense/Overview.jsx
+++ client/src/components/shared/Expense/Overview.jsx
@@ -59,6 +59,7 @@ const Overview = () => {
     const userId = user.id;
 
     const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+    console.log(`API URL: ${API_URL}`);
     // const API_URL = import.meta.env.VITE_API_URL;
 
     const [currentBalance, setCurrentBalance] = useState(0);
